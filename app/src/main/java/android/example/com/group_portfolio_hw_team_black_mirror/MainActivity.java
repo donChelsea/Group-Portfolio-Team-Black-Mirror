21d16884be79aaa5573096b6f78b27f563e3ccef
marliedee <marlyd364@gmail.com>
2018-12-08 18:05:36 -0500

    addedMarlyBio

---
+++ app/src/main/java/android/example/com/group_portfolio_hw_team_black_mirror/MainActivity.java
@@ -0,0 +1,69 @@
+package android.example.com.group_portfolio_hw_team_black_mirror;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private NavigationView navigationView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.chelsea:
+                        menuItem.setChecked(true);
+                        Intent chelseaIntent = new Intent(MainActivity.this, Biography.class);
+                        startActivity(chelseaIntent);
+                        drawerLayout.closeDrawers();
+                    default:
+                }
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ app/src/main/java/android/example/com/group_portfolio_hw_team_black_mirror/Marly_Biography.java
@@ -0,0 +1,71 @@
+package android.example.com.group_portfolio_hw_team_black_mirror;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+    public class Marly_Biography extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_marly__biography);
+
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.marly_menu, menu);
+            return true;
+        }
+
+
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item) {
+            switch (item.getItemId()) {
+
+                case R.id.option_bank:
+
+                    Uri uri1 = Uri.parse("https://github.com/marliedee/BankTellerDemo");
+                    Intent gitbank = new Intent(Intent.ACTION_VIEW,uri1);
+                    startActivity(gitbank);
+
+                    return true;
+
+                case R.id.option_cyoa:
+
+                    Uri uri2 = Uri.parse("https://github.com/marliedee/CYOA_Pursuit_HW_Dantes_Marly");
+                    Intent gitcyoa = new Intent(Intent.ACTION_VIEW,uri2);
+                    startActivity(gitcyoa);
+
+                    return true;
+
+                case R.id.option_madlibs:
+
+                    Uri uri3 = Uri.parse("https://github.com/marliedee/MadlibsApp");
+                    Intent gitmadlibs = new Intent(Intent.ACTION_VIEW,uri3);
+                    startActivity(gitmadlibs);
+
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+    }
